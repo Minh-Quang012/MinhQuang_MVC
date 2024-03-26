4a36d57a9cf80d70758357e1fe2a551c7eb81011
Nguyễn Minh Điền <diennm96@gmail.com>
2024-03-26 16:15:00 +0700

    Add project files.

---
+++ MinhQuangWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinhQuangWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
